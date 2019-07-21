fdb4711ae6e5aab5ec59fdc08cd9201d9b788eb5
rayGreycloud <rayGreycloud@gmail.com>
2019-07-21 08:31:07 -0700

    Video component

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import ReactPlayer from 'react-player';
 
-const Video = props => {
-  return (
-    <div>
-      <h2>Video</h2>
-    </div>
-  );
-};
+import StyledVideoWrapper from './StyledVideoWrapper';
+import StyledVideo from './StyledVideo';
 
+const Video = ({ active, autoplay, endCallback, progressCallback }) => (
+  <StyledVideo>
+    <StyledVideoWrapper>
+      <ReactPlayer
+        width='100%'
+        height='100%'
+        style={{ position: 'absolute', top: '0' }}
+        playing={autoplay}
+        controls={true}
+        url={active.video}
+        onEnded={endCallback}
+        onProgress={progressCallback}
+      />
+    </StyledVideoWrapper>
+  </StyledVideo>
+);
 export default Video;
