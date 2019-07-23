05bfc0917ebcfe94f098d0823bf88a4bf1925964
rayGreycloud <rayGreycloud@gmail.com>
2019-07-23 10:00:34 -0700

    add withLink to add title prop with embedded Link

--- src/components/withLink/index.jsx
+++ src/components/withLink/index.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const withLink = props => <div>withLink!!!</div>;
+const withLink = WrappedComponent => props => {
+  const newProps = {
+    ...props,
+    video: {
+      ...props.video,
+      title: (
+        <Link to={{ pathname: `/${props.video.id}`, autoplay: true }}>
+          {props.video.title}
+        </Link>
+      )
+    }
+  };
+
+  return <WrappedComponent {...newProps} />;
+};
 
 export default withLink;
